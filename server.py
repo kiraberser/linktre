92b916b77ce0a351df58c727dd45d06c1542432c
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-11-25 01:59:11 -0600

    Setting Port

--- server.py
+++ server.py
@@ -8,5 +8,5 @@ def home_page():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 6000))  # Usa el puerto asignado por Railway
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.getenv('PORT', 5000))  # Usa el puerto asignado por Railway
+    app.run(host='0.0.0.0', port=port)
